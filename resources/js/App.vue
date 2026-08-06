0dd52ab657cba9f303e00e8b97535dd6d1844921
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-06 16:07:29 -0400

    vue konfiguralasa

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Üdvözöl a Vue a Laravelben! 🚀</h1>
+        <p>Ez a komponens vette át a welcome.blade.php helyét.</p>
+    </div>
+</template>
+
+<script setup>
+// Ide jöhet a Vue 3 Composition API logika
+</script>
+
+<style scoped>
+.container {
+    text-align: center;
+    margin-top: 50px;
+    font-family: sans-serif;
+}
+h1 {
+    color: #42b883;
+}
+</style>
