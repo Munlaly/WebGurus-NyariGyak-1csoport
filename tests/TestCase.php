5e7c4d908d58bd76ed5f3a7203a375fbcf7f5b37
munlaly <nemetdbence@gmail.com>
2026-08-11 11:13:07 +0300

    fix test(auth): Tried to add a global beforeEach to Pest to solve the moch Vite issue

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,11 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-   protected function setUp(): void
-    {
-        parent::setUp();
-        
-        // Globally ignore missing Vite manifests during tests
-        $this->withoutVite();
-    }
+   
 }
