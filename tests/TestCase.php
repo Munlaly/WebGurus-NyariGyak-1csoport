ec599f284a5628af4ba2cae9fce167fc80f14c21
munlaly <nemetdbence@gmail.com>
2026-08-11 11:00:00 +0300

    fix(tests): globally disable Vite to resolve CI warnings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+   protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Globally ignore missing Vite manifests during tests
+        $this->withoutVite();
+    }
 }
