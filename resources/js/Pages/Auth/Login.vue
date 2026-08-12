c768f7668566c8008c4cde229ae49c31869a6330
munlaly <nemetdbence@gmail.com>
2026-08-12 08:55:39 +0300

    fix(login): Minimal boilerplate added to Login.vue so the pipeline can pass

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,1 +1,9 @@
-Login
+<script setup lang="ts">
+// Minimal script block to satisfy the TypeScript compiler
+</script>
+
+<template>
+  <div class="p-8">
+    <h1>Login Placeholder</h1>
+  </div>
+</template>
