130ce90a6fb33a90d7775999ab2edf5d389cbe50
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-13 11:17:08 -0400

    Add UserInventoryController, seeder, and routes for managing user inventory. Update User and UserInventory models, and modify migrations for users and personal access tokens.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\UserSettings;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,26 +13,12 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        $user = User::factory()->create([
-            'username' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-            'daily_calorie_target' => 2000,
-        ]);
-
-        UserSettings::create([
-            'user_id'=> $user->id,
-            'goals' => json_encode(['lose weight']),
-            'meal_plan_preference' => json_encode(['vegan']),
-            'household_size' => json_encode(['1 person']),
-            'prep_time_preference' => json_encode(['under 20 minutes']),
-            'budget_or_comfort' => 'comfort_first',
-        ]);
-        
+    {        
         $this->call ([
             CategorySeeder::class,
             RecipeSeeder::class,
+            UserSeeder::class,
+            UserInventorySeeder::class,
         ]);
     }
 }
