dc9fcf16b0eb8909b7116902092b11775fb32f0f
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-13 11:17:08 -0400

    Add UserInventoryController, seeder, and routes for managing user inventory. Update User and UserInventory models, and modify migrations for users and personal access tokens.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+<<<<<<< HEAD
 use App\Models\User;
 use App\Models\UserSettings;
 use Illuminate\Support\Facades\Hash;
+=======
+>>>>>>> d608328 (Add UserInventoryController, seeder, and routes for managing user inventory. Update User and UserInventory models, and modify migrations for users and personal access tokens.)
 
 class DatabaseSeeder extends Seeder
 {
