04472730e28e7d885801a22e4d322078f797c164
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-14 15:17:23 -0400

    fix: add ingredient and inventory typehints for phpstan test

--- app/Models/UserInventory.php
+++ app/Models/UserInventory.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserInventory extends Model
 {
@@ -22,7 +23,7 @@ class UserInventory extends Model
         'last_audited_at' => 'datetime',
     ];
 
-    public function ingredient() {
+    public function ingredient(): BelongsTo {
         return $this->belongsTo(Ingredient::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\UserInventoryController;
 use App\Http\Controllers\CookMealController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealPlanController;
-use App\Http\Controllers\UserRecipeController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/inventory', [UserInventoryController::class, 'index']);
@@ -15,8 +14,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/meal-plan/generate', [MealPlanController::class,'generate']);
     Route::post('/meal-plan/regenerate-meal', [MealPlanController::class,'regenerateMeal']);
     Route::post('/meal-plan/save', [MealPlanController::class,'savePlan']);
-    Route::post('/user/recipes', [UserRecipeController::class, 'store']);
-    Route::put('/user/recipes/{id}', [UserRecipeController::class, 'update']);
-
 });
     
