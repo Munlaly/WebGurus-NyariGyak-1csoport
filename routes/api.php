186ccfe690129276cb5d9334edd2f5702c68b782
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-14 13:46:02 -0400

    fix: remove CookMealController references that belong to another feature branch

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\UserInventoryController;
-use App\Http\Controllers\CookMealController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealPlanController;
 use App\Http\Controllers\UserRecipeController;
@@ -12,7 +11,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user/inventory', [UserInventoryController::class, 'store']);
     Route::put('/user/inventory', [UserInventoryController::class, 'update']);
     Route::delete('/user/inventory', [UserInventoryController::class, 'destroy']);
-    Route::post('/recipes/{id}/cook', [CookMealController::class, 'cook']);
     Route::post('/meal-plan/generate', [MealPlanController::class,'generate']);
     Route::post('/meal-plan/regenerate-meal', [MealPlanController::class,'regenerateMeal']);
     Route::post('/meal-plan/save', [MealPlanController::class,'savePlan']);
