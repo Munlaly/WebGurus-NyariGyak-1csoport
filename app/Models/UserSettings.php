8d29f5a68147855d6a24f0b859e04bd9c72ff7e9
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-14 14:39:37 -0400

    fix: update models, routes and config calls for settings branch

--- app/Models/UserSettings.php
+++ app/Models/UserSettings.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserSettings extends Model
 {
@@ -28,7 +29,7 @@ class UserSettings extends Model
         'custom_dislikes' => 'array',
     ];
 
-    public function user() {
+    public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
 }
