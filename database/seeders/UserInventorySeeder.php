f3000bf73b78edd41aa1099f5722c9ac494e1d0c
Sekeres Erik <erikszekeres3@gmail.com>
2026-08-24 09:34:40 -0400

    comment deleted in app.php and changing the logic in UserInventorySeeder from hard coded to find by email

--- database/seeders/UserInventorySeeder.php
+++ database/seeders/UserInventorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserInventorySeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class UserInventorySeeder extends Seeder
     {
         // 1. Clear the table first so we don't duplicate data on re-runs
         DB::table('user_inventories')->truncate();
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        if(! $user) {
+            return;
+        }
+
 
         // 2. These are the exact required amounts from your screenshot
         $recipeRequirements = [
@@ -26,7 +34,7 @@ class UserInventorySeeder extends Seeder
         // 3. Loop through and give the user enough of each ingredient
         foreach ($recipeRequirements as $ingredientId => $requiredAmount) {
             $inventoryData[] = [
-                'user_id' => 1, 
+                'user_id' => $user->id,
                 'ingredient_id' => $ingredientId, 
                 // We add 10 to the required amount so the user has leftovers after cooking
                 'amount_left' => $requiredAmount + 10, 
