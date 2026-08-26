fb33457d2b6cd96789065865bad22c5d7012ae47
munlaly <nemetdbence@gmail.com>
2026-08-26 16:16:59 +0300

    Moved adding daily_plan_id column to a new migration

--- database/migrations/2026_08_06_205301_create_meal_plans_table.php
+++ database/migrations/2026_08_06_205301_create_meal_plans_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('meal_plans', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('daily_plan_id')->constrained()->cascadeOnDelete();
             $table->foreignId('recipe_id')->constrained()->cascadeOnDelete();
             $table->enum('meal_type', ['breakfast', 'lunch', 'dinner', 'snack']);
             $table->enum('status', ['DRAFT', 'COMMITTED', 'EATEN', 'SKIPPED'])->default('DRAFT');
