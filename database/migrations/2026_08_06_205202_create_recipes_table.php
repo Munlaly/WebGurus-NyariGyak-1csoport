92d002a9888b44cb261e1a1ab43b094632686d03
munlaly <nemetdbence@gmail.com>
2026-08-27 13:29:43 +0300

    Fix: changed owner_id back to user_id

--- database/migrations/2026_08_06_205202_create_recipes_table.php
+++ database/migrations/2026_08_06_205202_create_recipes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('owner_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->string('name');
             $table->text('instructions')->nullable();
             $table->integer('prep_time_minutes')->nullable();
