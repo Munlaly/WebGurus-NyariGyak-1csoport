30b53d902ff9ca660fef30ce7e2c0fee8deb3adf
munlaly <nemetdbence@gmail.com>
2026-09-10 08:56:18 +0300

    Changed border to gray for warning on dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -407,7 +407,7 @@ watch(searchQuery, (newVal) => {
           title="No weekly plan yet"
           description="It looks like you haven't generated a meal plan for this week. Let's get you set up."
           :actions="[{ label: 'Go to Weekly Planner', onClick: goToPlanner }]"
-          class="border-error w-full border-2 border-dashed"
+          class="w-full border-2 border-dashed border-gray-600"
         />
       </div>
 
